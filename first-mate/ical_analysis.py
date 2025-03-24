f21b34a61cc8ae819df8449642ce4910f61495c3
Maddy Guthridge <hello@maddyguthridge.com>
2025-03-24 13:20:54 +1100

    Add ical_analysis.py template

---
+++ first-mate/ical_analysis.py
@@ -0,0 +1,29 @@
+"""
+ical_analysis.py
+
+Analyse an iCal file given its URL.
+"""
+
+
+from pathlib import Path
+
+
+async def download_ical(url: str) -> Path:
+    """
+    Download the given ical file and return its location.
+
+    It should be downloaded to the `icals` directory.
+
+    Parameters
+    ----------
+    url : str
+        URL for calendar to download
+
+    Path
+        Path to downloaded ical file on file system
+    """
+    ...
+
+
+# TODO: Code for processing ical files
+
