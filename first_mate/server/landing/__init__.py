d4e2fb2f754695bc8f342a009a7ec50cb89d7c2a
Maddy Guthridge <hello@maddyguthridge.com>
2025-03-26 04:49:03 +1100

    Move general css to root.css

--- first_mate/server/landing/__init__.py
+++ first_mate/server/landing/__init__.py
@@ -32,6 +32,7 @@ def generate_head() -> head:
         meta(charset="UTF-8"),
         meta(name="viewport", content="width=device-width, initial-scale=1.0"),
         title("FirstMate - Find Friends on Campus"),
+        link(rel="stylesheet", href="/static/root.css"),
         link(rel="stylesheet", href="/static/landing.css"),
     )
 
