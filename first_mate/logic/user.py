a2f703bd46125167206b09a2c5398c4a04c54343
Maddy Guthridge <hello@maddyguthridge.com>
2025-03-26 00:18:25 +1100

    Add profile page and profile edit page

--- first_mate/logic/user.py
+++ first_mate/logic/user.py
@@ -27,8 +27,11 @@ class User(TypedDict):
     display_name: str
     """Display name, shown to other users"""
 
-    profile_text: str
-    """Profile text, shown under name"""
+    public_description: str
+    """Public profile text, shown under name"""
+
+    private_description: str
+    """Private profile text, only shown to users who have been matched with"""
 
     password_hash: str
     """Hash for user's password"""
@@ -134,7 +137,8 @@ def register_user(
     user_data: User = {
         "zid": zid,
         "display_name": display_name,
-        "profile_text": "",
+        "public_description": "",
+        "private_description": "",
         "password_hash": hashed,
         "password_salt": salt,
         "calendar": download_ical(ical_url),
--- first_mate/server/profile.py
+++ first_mate/server/profile.py
@@ -8,6 +8,7 @@ import pyhtml as p
 from hashlib import sha256
 from flask import Blueprint, redirect, request
 
+from first_mate.logic.data import save_data
 from first_mate.logic.user import get_user_by_zid
 from first_mate.server.session import get_user, is_user_logged_in
 from first_mate.server.util import (
@@ -37,9 +38,11 @@ def profile_image(zid: str, username: str) -> p.img:
     """
     email = f"{zid}@ad.unsw.edu.au"
     hash = sha256(email.encode()).hexdigest()
+    size = 200
     return p.img(
-        src=f"https://gravatar.com/avatar/{hash}",
+        src=f"https://gravatar.com/avatar/{hash}?s={size}",
         alt=f"Avatar image for {username}",
+        class_="profile-image",
     )
 
 
@@ -54,8 +57,8 @@ def profile_root():
 
 @profile.get("/<zid>")
 def profile_page(zid: str):
-    user = get_user_by_zid(zid)
-    if user is None:
+    user_to_view = get_user_by_zid(zid)
+    if user_to_view is None:
         return str(
             error_page(
                 "Profile - Error 404",
@@ -63,7 +66,64 @@ def profile_page(zid: str):
                 "User not found",
                 is_user_logged_in(),
             )
+        ), 404
+
+    public_profile_text = p.div(class_="profile-description")(
+        multiline_str_to_html(user_to_view["public_description"])
+        if user_to_view["public_description"]
+        else p.i("This user has not added a profile description")
+    )
+
+    me = get_user()
+
+    # We are that user
+    # TODO: OR if we have matched with the user
+    if me and zid == me["zid"]:
+        private_profile_text = p.div(class_="profile-description")(
+            [multiline_str_to_html(user_to_view["private_description"])]
+            if user_to_view["private_description"]
+            else [p.i("This user has not added a private profile description")]
         )
+    else:
+        private_profile_text = []
+
+    if me and zid == me["zid"]:
+        edit_option = [p.a(href=f"/profile/{zid}/edit")("Edit profile")]
+    else:
+        edit_option = []
+
+    return str(
+        p.html(
+            p.head(
+                p.title(f"Profile - {user_to_view['display_name']}"),
+                p.link(href="/static/root.css", rel="stylesheet"),
+            ),
+            p.body(
+                navbar(True),
+                p.h1(user_to_view["display_name"]),
+                profile_image(zid, user_to_view["display_name"]),
+                public_profile_text,
+                private_profile_text,
+                edit_option,
+            ),
+        )
+    )
+
+
+@profile.get("/<zid>/edit")
+def profile_edit_page(zid: str):
+    user = get_user()
+    if user is None:
+        return redirect("/auth/login")
+    if user["zid"] != zid:
+        return str(
+            error_page(
+                "Edit Profile - Error 403",
+                "Error 403",
+                "You cannot edit a profile that isn't yours",
+                True,
+            )
+        ), 403
 
     return str(
         p.html(
@@ -73,9 +133,71 @@ def profile_page(zid: str):
             ),
             p.body(
                 navbar(True),
-                p.h1(user["display_name"]),
+                p.h1("Edit profile"),
                 profile_image(zid, user["display_name"]),
-                multiline_str_to_html(user["profile_text"]),
+                p.i("You can edit your profile picture using Gravatar"),
+                p.form(
+                    # Main profile edit
+                    # Name
+                    p.label(for_="edit-name")(p.p("Display name")),
+                    p.input(
+                        id="edit-name",
+                        name="name",
+                        placeholder="Display name",
+                        value=user["display_name"],
+                        required=True,
+                    ),
+                    # Profile description
+                    p.label(for_="edit-public-description")(
+                        p.p("Public profile description. This is shown to all users."),
+                    ),
+                    p.textarea(style="width: 100%; height: 200px;")(
+                        id="edit-public-description",
+                        name="public_description",
+                        placeholder="Your public profile description",
+                    )(user["public_description"]),
+                    p.label(for_="edit-private-description")(
+                        p.p(
+                            "Public profile description. This is only shown to "
+                            "users who you have matched with."
+                        ),
+                    ),
+                    p.textarea(style="width: 100%; height: 200px;")(
+                        id="edit-private-description",
+                        name="private_description",
+                        placeholder="Your private profile description",
+                    )(user["private_description"]),
+                    # TODO: Degrees
+                    # Submit
+                    p.input(type="submit", value="Save"),
+                ),
             ),
         )
     )
+
+
+@profile.post("/<zid>/edit")
+def profile_edit_submit(zid: str):
+    user = get_user()
+    if user is None:
+        return redirect("/auth/login")
+    if user["zid"] != zid:
+        return str(
+            error_page(
+                "Edit Profile - Error 403",
+                "Error 403",
+                "You cannot edit a profile that isn't yours",
+                True,
+            )
+        ), 403
+
+    display_name = request.form["name"]
+    public_description = request.form["public_description"]
+    private_description = request.form["private_description"]
+
+    user["display_name"] = display_name
+    user["public_description"] = public_description
+    user["private_description"] = private_description
+    save_data()
+
+    return redirect(f"/profile/{zid}")
