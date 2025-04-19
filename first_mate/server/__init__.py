ea796a93aa117847282a04053c18b6ba7929a413
Maddy Guthridge <hello@maddyguthridge.com>
2025-04-19 19:16:52 +1000

    Move static content to fix issue with static file serving

--- first_mate/server/__init__.py
+++ first_mate/server/__init__.py
@@ -5,7 +5,7 @@ Entrypoint to the first-mate server.
 """
 
 import os
-from flask import Flask, send_file
+from flask import Flask
 
 from first_mate.consts import dev
 from first_mate.server.landing import render_page
@@ -34,17 +34,3 @@ if dev():
 def root():
     logged_in = is_user_logged_in()
     return render_page(logged_in)
-
-
-@app.get("/static/<filename>")
-def serve_static(filename):
-    """
-    Manually implement static directory because we couldn't convince Flask to
-    actually load the data no matter what we tried :(
-
-    Seriously I do not know why it was broken no matter what we did
-
-    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-    """
-    file_path = os.path.join(os.getcwd(), "first_mate", "static", filename)
-    return send_file(file_path)
